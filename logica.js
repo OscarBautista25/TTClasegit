5c003fb19674467bb3b36a4fec2126e230870c1e
Oscar Bautista <ingenierobautistaoscar@gmail.com>
2024-04-08 20:04:50 -0500

    Se agrega un cambio para ver en remoto

--- logica.js
+++ logica.js
@@ -1,3 +1,4 @@
 alert("Bienvenidos a Taller Tecnológico II")
 console.log("Esto que estoy escribiendo se verá en la consola de HTML")
 console.log("Esta va ser la tercera versión")
+console.log("Este cambio irá hasta remoto")
